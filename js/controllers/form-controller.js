57d6f0bf3e27554e521bf5fe290adbe8a87056bd
Guilherme Assis <contato.guilhermeassis@gmail.com>
2022-12-26 13:20:37 -0300

    construindo a consulta cep

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,6 +1,9 @@
-
+import Address from "../models/address.js";
+import * as requestService from '../services/request-service.js'
 
 function State() {
+
+    this.address = new Address();
 
     this.btnSave = null;
     this.btnClear = null;
@@ -12,7 +15,6 @@ function State() {
 
     this.errorCep = null;
     this.errorNumber = null;
-
 }
 
 const state = new State ()
@@ -30,6 +32,46 @@ export function init() {
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
-    console.log(state);
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+
+}
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault();
+    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(result);
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm() {
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.inputCep.focus();
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}]`);
+    element.innerHTML = value;
 
 }
